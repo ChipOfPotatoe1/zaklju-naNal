fc83bc893dfe33125a2aa36d5374f89475c84501
ChipOfPotatoe1 <jure.udir2007@gmail.com>
2025-04-16 08:44:59 +0200

    commit

--- krneki.py
+++ krneki.py
@@ -1,23 +1,47 @@
 import requests
+from flask import Flask, render_template, request
 
 API_kljuc_igre = "7482e2d4fb924a119eedc34862b5ce39"
 URL_igre = "https://api.rawg.io/api"
+
+app = Flask(__name__)
 
 #iskanje iger
 def iskanje_iger(ime_igre):
     url = f"{URL_igre}/games?key={API_kljuc_igre}&search={ime_igre}" #link z formatiranimi podatki
     podatki = requests.get(url) #pridobivanje podatkov
-    vrnjeno = podatki.json() #spreminjanje za lepsi pogled
+    vrnjeno = podatki.json() #spreminjanje v json datoteko
     return vrnjeno #vračanje json datoteke
 
-#pridobivanje podatkov
-ime_igre = input("vnesi ime: ")
-podatki = iskanje_iger(ime_igre)
+#iskanje opisa igre
+def iskanje_opisa(ID):
+    url = f"{URL_igre}/games/{ID}?key={API_kljuc_igre}"
+    podatki = requests.get(url)
+    vrnjeno = podatki.json()
+    return vrnjeno
 
-#podatki
-ime = podatki['results'][0]['name']
-datum = podatki['results'][0]['released']
-ocena = podatki['results'][0]['rating']
-slika = podatki['results'][0]['background_image']
+@app.route('/', methods=['GET'])
+def index():
+    #pridobivanje podatkov
+    ime_igre = request.args.get('ime')
+    podatki = iskanje_iger(ime_igre)
+    
+    if ime_igre: #če je vnešeno ime igre se prikažejo podatki
+        #podatki
+        ime = podatki['results'][0]['name']
+        datum = podatki['results'][0]['released']
+        ocena = podatki['results'][0]['rating']
+        slika = podatki['results'][0]['background_image']
+        ID = podatki['results'][0]['id']
 
-print(podatki['results'][0]['background_image'])
+        #podatki o opisu igre
+        podatkiOpis = iskanje_opisa(ID)
+        opis = podatkiOpis['description']
+        print(ime, datum, ocena, slika, ID)
+        return render_template('test.html', ime = ime, datum = datum, ocena = ocena, slika = slika, opis = opis)
+    
+    else: # če ni vnešeno (else) se prikaže samo vrstica za iskanje, če tega if stavka ni se prikažejo privzete vrednosti
+        return render_template('test.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
