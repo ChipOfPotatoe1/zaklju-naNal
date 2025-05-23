5a281bd37188743f3d82de4a4ddee70b80063082
ChipOfPotatoe1 <jure.udir2007@gmail.com>
2025-05-23 10:25:10 +0200

    dodajanje kviza

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+import requests, random
 from flask import Flask, render_template, request, redirect, url_for, session
 from tinydb import TinyDB, Query
 
@@ -122,6 +122,54 @@ def serije():
         return render_template('serije.html', ime = ime, jezik = jezik, slika = slika, ocena = ocena, opis = opis, datum = datum)
     else:
         return render_template('serije.html')
+
+#kviz o igrah, da stran ne bo tako dougočasna
+@app.route('/kviz', methods=['GET', "POST"])
+def kviz():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+        
+    if method == 'POST':    #če smo vnesli imena se to izvede
+        #odgovori
+        odgovor1 = request.form['ime1'].strip().lower() #strip in lower odstranita vse okoli besed pa vse črke spremenita v male za vsak slučaj če uporabnik napiše ime z malo
+        odgovor2 = request.form['ime2'].strip().lower()
+        odgovor3 = request.form['ime3'].strip().lower()
+
+        #pravilni odgovori
+        pravilno1 = request.form['pravilno1'].strip().lower()
+        pravilno2 = request.form['pravilno2'].strip().lower()
+        pravilno3 = request.form['pravilno3'].strip().lower()
+
+        i = 0
+        if odgovor1 == pravilno1:
+            i += 1
+        if odgovor2 == pravilno2:
+            i += 1
+        if odgovor3 == pravilno3:
+            i += 1
+    st = random.randint(3, 21) #izbira random cifro za igro
+    url = f"{URL_igre}/games?key={API_kljuc_igre}&page={st}&page_size=30"
+    podatki = requests.get(url)
+    igre = podatki.json()['results']
+    igra1 = random.choice(igre) #izbere prvo igro
+    igra2 = random.choice(igre) #izbere drugo igro
+    igra3 = random. choice(igre) #izbere tretjo igro
+    
+    #slika in ime za prvo igro
+    slika1 = igra1["background_image"]
+    ime1 = igra1["name"]
+    #slika in ime za drugo igro
+    slika2 = igra2["background_image"]
+    ime2 = igra2["name"]
+
+    #slika in ime za tretjo igro
+    slika3 = igra3["background_image"]
+    ime3 = igra3["name"]
+    #print(igra1)
+    print(slika1)
+    print(slika2)
+    print(slika3)
+    return render_template('kviz.html', slika1 = slika1, slika2 = slika2, slika3 = slika3)
 
 if __name__ == "__main__":
     app.run(debug=True)
